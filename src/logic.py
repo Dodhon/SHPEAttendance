8f0cae1c8168ef4443967c21f47f7cf8c0d7cd00
Thupten Wangpo <110739214+Dodhon@users.noreply.github.com>
2024-08-28 22:28:52 -0500

    poc done

--- src/logic.py
+++ src/logic.py
@@ -1,51 +1,42 @@
 import pandas as pd
-import os, sys
-import csv
+import os
 
+def process_emails(emails):
+    return [email.lower() for email in emails]
 
-allPeople = set()
-masterDf = pd.DataFrame
+def getFiles(path="/Users/thuptenwangpo/Documents/GitHub/SHPEAttendance/attendanceFormResponses/"):
+    return [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.csv')]
 
-def createTable():
-    
-    createNamesColumn(masterDf)
-    dfs = list()
-    
-
-def updateTable():
-    files = getFiles()
-    dfs = list()
+def createMasterAttendance(files):
+    masterDf = pd.DataFrame()
     for file in files:
-        dfs.append(pd.read_csv(file)) ## dfs is now a list of dataframes. Each data frame is one attendance form 
-    for df in dfs:
-        allPeople.add(getEmails(df))
+        df = pd.read_csv(file)
 
-def createEmailColumn(df):
-    if "Hawk Email" not in df:
-            df.insert(0,"Hawk Email", allPeople)
+        event_name = os.path.splitext(os.path.basename(file))[0]
 
-def getEmails(df): ## will return list of all names of people that attended a given event
-    emails = list()
-    for e in df["Hawk Email"]:
-        pass
-    return emails
-def processName(name):
-    return str(name).lower.replace(" ", "")
+        emailCol = next((col for col in df.columns if "email" in col.lower()), None)
+        if emailCol:
+            df.rename(columns={emailCol: "Email"}, inplace=True)
+        else:
+            print(f"Warning: No email column found in {file}. Skipping...")
+            continue 
 
+        df['Email'] = process_emails(df['Email'])
+        df['Event'] = event_name  
+        masterDf = pd.concat([masterDf, df[['Email', 'Event']]], ignore_index=True)
 
+    return masterDf
 
+def createBinaryMatrix(masterDf):
+    binaryMatrix = masterDf.pivot_table(index='Email', columns='Event', aggfunc='size', fill_value=0)
+    binaryMatrix = binaryMatrix.applymap(lambda x: 1 if x > 0 else 0)
+    return binaryMatrix
 
+if __name__ == "__main__":
+    files = getFiles()
+    master_attendance = createMasterAttendance(files)
+    binaryMatrix = createBinaryMatrix(master_attendance)
 
-def getFiles(): ## returns a list of all attendance files
-    path = "attendanceFormResponses"
-    dirs = os.listdir(path)
-    csvFiles = list()
-
-    for file in dirs:
-        try:
-            file == '*.csv'
-            csvFiles.append(file)
-        except:
-            os.remove(file)
-    
-    return csvFiles
+    output_file = "/Users/thuptenwangpo/Documents/GitHub/SHPEAttendance/masterAttendanceSheet/masterAttendance.csv"
+    binaryMatrix.to_csv(output_file, index=True)
+    print(f"Binary attendance matrix saved to {output_file}")
