cf6ec70fc061ca19b218fc2c37e8271361956426
Thupten Wangpo <110739214+Dodhon@users.noreply.github.com>
2025-02-05 14:37:53 -0600

    added loop to create multiples qr codes at once

--- src/qrCodeGen.py
+++ src/qrCodeGen.py
@@ -2,9 +2,9 @@ import requests
 import os
 
 # Data to encode
-data = "https://www.canva.com/design/DAGXEoTB1B0/Li7bc7Smhk-k3fiRTSxedA/view?utm_content=DAGXEoTB1B0&utm_campaign=share_your_design&utm_medium=link&utm_source=shareyourdesignpanel"
+data = input("please paste qr code link: ")
 directory = "/Users/thuptenwangpo/Documents/GitHub/SHPEAttendance/qr_codes"
-title = "FCN Form"
+title = input("Please enter the title of the qr code: ")
 
 
 # API endpoint
@@ -22,3 +22,32 @@ image_path = os.path.join(directory, f"qrcode_{title}.png")
 # Save the image
 with open(image_path, 'wb') as f:
     f.write(response.content)
+    
+    
+# Ask if the user wants to stop
+stop = "n"
+while not stop:
+    stop = input("Do you want to stop? (y/n): ")
+    if stop.lower() == "y":
+        stop = True
+    else:
+        data = input("please paste qr code link: ")
+        directory = "/Users/thuptenwangpo/Documents/GitHub/SHPEAttendance/qr_codes"
+        title = input("Please enter the title of the qr code: ")
+
+
+        # API endpoint
+        api_endpoint = f"https://api.qrserver.com/v1/create-qr-code/?size=300x300&data={data}"
+
+
+        # Fetch the QR code image
+        response = requests.get(api_endpoint)
+
+
+        # Full path for saving the image
+        image_path = os.path.join(directory, f"qrcode_{title}.png")
+
+
+        # Save the image
+        with open(image_path, 'wb') as f:
+            f.write(response.content)
